a3b357964d8d096b2518da2eae0af94dde3041aa
Administrator <513523798@qq.com>
2025-01-28 15:40:55 +0800

    重载

---
+++ day02/src/type.java
@@ -0,0 +1,21 @@
+package day02.src;
+
+public class type {
+    public static void main(String[] args) {
+        int result = calc2((byte)110,(byte)120);
+        System.out.println(result);
+
+        int result2 = calc3((byte)110,(byte)120);
+        System.out.println(result2);
+    }
+    public static double calc(int a, int b, double c, char r){
+        return a+b+c+r;
+    }
+    public static int calc2(byte a, byte b){
+        return a+b;
+    }
+    public static byte calc3(byte a, byte b){
+        return (byte) (a+b);
+    }
+}
+
